43d9751777e8699b32834f3623e88559cf1e3c77
thatt03 <123438005+thatt03@users.noreply.github.com>
2023-01-24 15:31:02 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -7,12 +7,12 @@ addLayer("p", {
 		points: new Decimal(0),
     }},
     color: "#0096FF",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    requires: new Decimal(1), // Can be a function that takes requirement increases into account
     resource: "Knowledge", // Name of prestige currency
     baseResource: "Mana", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 0.4, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -29,8 +29,28 @@ addLayer("p", {
     upgrades: {
         11: {
             title: "Expansive Research",
-            description: "Double your Mana gain.",
-            cost: new Decimal(1),
+            description: "Triple your Mana gain.",
+            cost: new Decimal(2),
+        },
+
+        12: {
+            title: "Harness Knowledge",
+            description: "Increase Mana gain by current knowledge",
+            cost: new Decimal(2),
+            effect() {
+                return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
+        },
+        
+        13: {
+            title: "test",
+            description: "test",
+            cost: new Decimal(3),
+            effect() {
+                return player.points.add(1).pow(0.15)
+            },  
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
         },
     }
 })
