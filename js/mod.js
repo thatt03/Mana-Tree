e3b927fc1dfb7a76378aefcf64e4f6e63acb3e76
thatt03 <123438005+thatt03@users.noreply.github.com>
2023-01-24 00:53:45 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,11 +1,11 @@
 let modInfo = {
-	name: "The ??? Tree",
+	name: "Mana Tree",
 	id: "mymod",
-	author: "nobody",
+	author: "Thomas H",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
-	discordName: "",
+	discordName: "hkd29#6455",
 	discordLink: "",
 	initialStartPoints: new Decimal (10), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
