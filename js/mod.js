cef9c4e020595f842b0b1c914fe892009aeb8961
thatt03 <123438005+thatt03@users.noreply.github.com>
2023-01-25 12:44:53 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -7,7 +7,7 @@ let modInfo = {
 
 	discordName: "hkd29#6455",
 	discordLink: "",
-	initialStartPoints: new Decimal (0), // Used for hard resets and new players
+	initialStartPoints: new Decimal (1), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
 }
 
